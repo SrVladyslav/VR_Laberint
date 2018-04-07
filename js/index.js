d73b7b42b8df0e77b24d2d5f4f66624b587f2890
duxy1996 <durancdr96@gmail.com>
2018-04-07 11:57:14 +0200

    Implemented function to set boxes in a xyz point

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log("Three JS");
+window.onload = function () { main() }
+
+function main() {
+  var sceneEl = document.querySelector('a-scene');
+
+  /*
+    This method adds a box to the scene in the scene
+    declared in the html
+  */
+  function add_box(x,y,z){
+    var sphere = document.createElement('a-box');
+    sphere.setAttribute('geometry', {
+      depth: 1,
+      width: 1,
+      height: 1
+    });
+    sphere.setAttribute('position', {x: x, y: y, z: z});
+    sphere.setAttribute('material', 'color', 'red');
+    sphere.setAttribute('shadow','cast','true');
+    sceneEl.appendChild(sphere);
+  }
+
+  add_box(2,2,2);
+}
