c9fbe3c652e9383d622c0d83e4b183580e85329c
SrVladyslav <srvladys@gmail.com>
2018-04-27 19:17:25 +0200

    V0.01

--- js/index.js
+++ js/index.js
@@ -1,24 +1,46 @@
-window.onload = function () { main() }
+window.onload = function () { main()}
 
 function main() {
-  var sceneEl = document.querySelector('a-scene');
+  var mapa = document.querySelector('a-scene');
 
+  var tablaJuego = new Array(10);
+
+  for(var i= 0; i < 10; i++){
+    tablaJuego[i] = i;
+  }
   /*
-    This method adds a box to the scene in the scene
-    declared in the html
+  alert(""+tablaJuego[5]);*/
+  /*
+    Este metodo añade un cubo al mapa para construir el laberinto
+    @param: x,y: coordenadas respectivas
   */
-  function add_box(x,y,z){
-    var sphere = document.createElement('a-box');
-    sphere.setAttribute('geometry', {
+
+  function pared(x,y,z){
+    var muro = document.createElement('a-box');         //creo el cubo que sera nuestro muro
+    muro.setAttribute('geometry', {
       depth: 1,
       width: 1,
       height: 1
     });
-    sphere.setAttribute('position', {x: x, y: y, z: z});
-    sphere.setAttribute('material', 'color', 'red');
-    sphere.setAttribute('shadow','cast','true');
-    sceneEl.appendChild(sphere);
+    muro.setAttribute('position', {x: x,y: y,z: z});    //declaramos la posicion
+    muro.setAttribute('shadow','cast','true');          //declaramos la sombra
+    muro.setAttribute('material',{src:"assets/piedra.jpg"});
+    mapa.appendChild(muro);                             //añado mi muro al mapa
   }
+  laberinto(50,50); 
 
-  add_box(2,2,2);
+  function laberinto(ancho, largo){
+    //dibujo las paredes exteriores que limitaran el campo
+    for(var i = 0;i < ancho;i++){pared(-1,0,i - 1); pared(largo - 1,0,i - 1);} 
+    for(var j = 0;j < largo;j++){pared(j-1,0,0 - 1); pared(j - 1,0,ancho - 1);} 
+    //aplicando el algoritmo de generacion del laberinto
+    for(var i = 1; i < ancho; i++){
+        for(var j = 1; j < largo; j++){
+            var dibujar = Math.random()* (1 - 0) + 0;
+            if(dibujar >= 0.5){
+              pared(i - 1,0,j - 1);
+            }
+        }
+    }
+  }
 }
