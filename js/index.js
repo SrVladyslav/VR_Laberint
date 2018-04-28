776b5c3a7689a89053404edd0dcbe59afc09da6a
SrVladyslav <srvladys@gmail.com>
2018-04-28 23:23:13 +0200

    V0.051

--- js/index.js
+++ js/index.js
@@ -1,15 +1,6 @@
 window.onload = function () { main()}
 
-AFRAME.registerComponent('static-movement', {
-    schema: {default: ''},
-    init: function () {
-       var el = this.el;
-       el.addEventListener('click', function () {
-          document.querySelector('#player').setAttribute('position', el.getAttribute('position'));
-          console.log("Click: Player moved");
-       });
-    }
-});
+
 
 function main() {
   var mapa = document.querySelector('a-scene');
